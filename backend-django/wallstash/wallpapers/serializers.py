401c396e0d12cb94b2bfbd19c78d7ae358ea29f6
Pachev_Danila <pachev-danila@mail.ru>
2026-02-02 12:30:46 +0300

    feat: add first name and last name fields to CommentSerializer

--- backend-django/wallstash/wallpapers/serializers.py
+++ backend-django/wallstash/wallpapers/serializers.py
@@ -64,13 +64,23 @@ class CommentSerializer(serializers.ModelSerializer):
         source="user.username",
         read_only=True
     )
+    first_name = serializers.CharField(
+        source="user.first_name",
+        read_only=True
+    )
+    last_name = serializers.CharField(
+        source="user.last_name",
+        read_only=True
+    )
+
     class Meta:
         model = Comment
         fields = [
             "id",
             "username",
-            "wallpaper",
+            "first_name",
+            "last_name",
             "text",
             "created_at"
         ]
-        read_only_fields = ["user", "wallpaper"]
+        read_only_fields = ["created_at", "id"]
